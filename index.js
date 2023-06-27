d14d28aabeb7b81eaa3b946dcb0b78723504ec66
Owens-Robert <owensed18@gmail.com>
2023-06-27 15:07:15 -0500

    Day 2 of Portfolio

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("''''''''''''''''''''''''''")
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    console.log("hello----------")
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
